70da1af9885cde56b499ea3c3cc4a0438cc975ef
Kwon <anstjdrnjs3@gmail.com>
2022-05-19 23:47:41 +0900

    TypeOrm setting

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,54 @@
 import { MiddlewareConsumer, NestModule } from '@nestjs/common';
+
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as Joi from 'joi';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerMiddleware } from './common/middlewares/logger.middleware';
+import { UsersModule } from './users/users.module';
+
+const typeOrmModuleOptions = {
+  useFactory: async (): Promise<TypeOrmModuleOptions> => ({
+    namingStrategy: new SnakeNamingStrategy(),
+    type: 'mysql',
+    host: process.env.DB_HOST,
+    port: +process.env.DB_PORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    entities: [],
+    synchronize: true,
+    autoLoadEntities: true,
+    logging: true,
+    keepConnectionAlive: true,
+  }),
+};
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test', 'provision')
+          .default('development'),
+        PORT: Joi.number().default(5000),
+        SECRET_KEY: Joi.string().required(),
+        ADMIN_USER: Joi.string().required(),
+        ADMIN_PASSWORD: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_NAME: Joi.string().required(),
+      }),
+    }),
+    TypeOrmModule.forRootAsync(typeOrmModuleOptions),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
