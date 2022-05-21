ac82215dda9ffe7f116ebd9daa08abe04a64eb53
Kwon <anstjdrnjs3@gmail.com>
2022-05-21 18:09:13 +0900

    common entity

--- server/src/common/entities/common.entity.ts
+++ server/src/common/entities/common.entity.ts
@@ -1,8 +1,30 @@
+import { Exclude } from 'class-transformer';
 import { IsUUID } from 'class-validator';
-import { PrimaryGeneratedColumn } from 'typeorm';
+import {
+  CreateDateColumn,
+  DeleteDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 export abstract class CommonEntity {
   @IsUUID()
   @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @CreateDateColumn({
+    type: 'timestamp',
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamp',
+  })
+  updateAt: Date;
+
+  @Exclude()
+  @DeleteDateColumn({
+    type: 'timestamp',
+  })
+  deletedAt?: Date | null;
 }
