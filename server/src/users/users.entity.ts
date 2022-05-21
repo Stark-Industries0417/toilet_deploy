1598d3d43ec84648dd8c5c0e26556c5a5918dd7b
Kwon <anstjdrnjs3@gmail.com>
2022-05-21 18:09:32 +0900

    user entity

--- server/src/users/users.entity.ts
+++ server/src/users/users.entity.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Exclude } from 'class-transformer';
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 import { CommonEntity } from 'src/common/entities/common.entity';
@@ -8,21 +9,43 @@ import { Column, Entity, Index } from 'typeorm';
   name: 'USER',
 })
 export class UserEntity extends CommonEntity {
+  @ApiProperty({
+    example: 'abc@naver.com',
+    description: 'email',
+    required: true,
+  })
   @IsEmail({}, { message: '올바른 이메일을 작성해주세요.' })
   @IsNotEmpty({ message: '이메일을 작성해주세요.' })
   @Column({ type: 'varchar', unique: true, nullable: false })
   email: string;
 
+  @ApiProperty({
+    example: 'stark123!',
+    description: 'password',
+    required: true,
+  })
   @Exclude()
-  @Column({ type: 'varchar', nullable: false })
+  @Column({
+    select: false,
+    type: 'varchar',
+    nullable: false,
+  })
   password: string;
 
+  @ApiProperty({
+    example: 'stark',
+    description: 'nickname',
+    required: true,
+  })
   @IsString()
   @IsNotEmpty({ message: '닉네임을 작성해주세요.' })
-  @Column({ type: 'varchar', nullable: false })
+  @Column({ type: 'varchar', unique: true, nullable: false })
   nickname: string;
 
   @IsString()
-  @Column({ type: 'varchar' })
+  @Column({
+    type: 'varchar',
+    default: 'https://t1.daumcdn.net/cfile/tistory/2513B53E55DB206927',
+  })
   imgUrl: string;
 }
