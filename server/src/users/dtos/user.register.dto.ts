d5a007f2e7e8468e0709b71cd4cda3acbf2dec5c
Kwon <anstjdrnjs3@gmail.com>
2022-05-28 15:53:41 +0900

    modify validator

--- server/src/users/dtos/user.register.dto.ts
+++ server/src/users/dtos/user.register.dto.ts
@@ -1,4 +1,5 @@
 import { ApiProperty, PickType } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
 import { UserEntity } from '../users.entity';
 
 export class UserRegisterDto extends PickType(UserEntity, [
@@ -6,6 +7,7 @@ export class UserRegisterDto extends PickType(UserEntity, [
   'password',
   'nickname',
 ] as const) {
+  @IsString()
   @ApiProperty({
     example: 'stark123!',
     description: 'check password',
