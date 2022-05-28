a67bbb87ba0de6c63e1af71d408578a4355b659e
Kwon <anstjdrnjs3@gmail.com>
2022-05-28 15:54:51 +0900

    exprot typeorm module

--- server/src/users/users.module.ts
+++ server/src/users/users.module.ts
@@ -1,12 +1,17 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from 'src/auth/auth.module';
 import { UsersController } from './controllers/users.controller';
 import { UsersService } from './services/users.service';
 import { UserEntity } from './users.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity])],
+  imports: [
+    TypeOrmModule.forFeature([UserEntity]),
+    forwardRef(() => AuthModule),
+  ],
   controllers: [UsersController],
   providers: [UsersService],
+  exports: [TypeOrmModule.forFeature([UserEntity])],
 })
 export class UsersModule {}
