f440f0a2568f1c0c2c139c74ba460c685ac260a1
Kwon <anstjdrnjs3@gmail.com>
2022-05-31 19:31:51 +0900

    remove the image deletion function

--- server/src/aws.service.ts
+++ server/src/aws.service.ts
@@ -1,18 +1,15 @@
 import * as path from 'path';
-import * as AWS from 'aws-sdk';
+import S3 = require('aws-sdk/clients/s3');
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { PromiseResult } from 'aws-sdk/lib/request';
-
-// sharp
 
 @Injectable()
 export class AwsService {
-  private readonly awsS3: AWS.S3;
+  private readonly awsS3: S3;
   public readonly S3_BUCKET_NAME: string;
 
   constructor(private readonly configService: ConfigService) {
-    this.awsS3 = new AWS.S3({
+    this.awsS3 = new S3({
       accessKeyId: this.configService.get('AWS_S3_ACCESS_KEY'),
       secretAccessKey: this.configService.get('AWS_S3_SECRET_KEY'),
       region: this.configService.get('AWS_S3_REGION'),
@@ -25,46 +22,15 @@ export class AwsService {
     file: Express.Multer.File,
   ): Promise<{
     key: string;
-    s3Object: PromiseResult<AWS.S3.PutObjectOutput, AWS.AWSError>;
     contentType: string;
   }> {
     try {
       const key = `${folder}/${Date.now()}_${path.basename(
         file.originalname,
       )}`.replace(/ /g, '');
-
-      const s3Object = await this.awsS3
-        .putObject({
-          Bucket: this.S3_BUCKET_NAME,
-          Key: key,
-          Body: file.buffer,
-          ACL: 'public-read',
-          ContentType: file.mimetype,
-        })
-        .promise();
-      return { key, s3Object, contentType: file.mimetype };
+      return { key, contentType: file.mimetype };
     } catch (error) {
       throw new BadRequestException(`File upload failed : ${error}`);
-    }
-  }
-
-  async deleteS3Object(
-    key: string,
-    callback?: (err: AWS.AWSError, data: AWS.S3.DeleteObjectOutput) => void,
-  ): Promise<{ success: true }> {
-    try {
-      await this.awsS3
-        .deleteObject(
-          {
-            Bucket: this.S3_BUCKET_NAME,
-            Key: key,
-          },
-          callback,
-        )
-        .promise();
-      return { success: true };
-    } catch (error) {
-      throw new BadRequestException(`Failed to delete file : ${error}`);
     }
   }
 
