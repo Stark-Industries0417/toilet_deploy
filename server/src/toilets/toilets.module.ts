9ae7aace9773720959e55a87aefce6068c1d649c
Kwon <anstjdrnjs3@gmail.com>
2022-06-12 22:10:28 +0900

    import TypeOrm about toilet

--- server/src/toilets/toilets.module.ts
+++ server/src/toilets/toilets.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from 'src/users/users.module';
 import { ToiletsController } from './controllers/toilets.controller';
 import { ToiletsService } from './services/toilets.service';
+import { ToiletEntity } from './toilets.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ToiletEntity]), UsersModule],
   controllers: [ToiletsController],
   providers: [ToiletsService],
 })
