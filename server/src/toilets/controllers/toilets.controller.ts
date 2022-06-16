294db5f3cde433bd6f70605abbc0330191e99392
Kwon <anstjdrnjs3@gmail.com>
2022-06-16 14:38:24 +0900

    화장실 추가 api controller

--- server/src/toilets/controllers/toilets.controller.ts
+++ server/src/toilets/controllers/toilets.controller.ts
@@ -1,4 +1,27 @@
+import { Body } from '@nestjs/common';
+import { UseInterceptors } from '@nestjs/common';
+import { UseGuards } from '@nestjs/common';
+import { Post } from '@nestjs/common';
 import { Controller } from '@nestjs/common';
+import { userInfo } from 'os';
+import { JwtAuthGuard } from 'src/auth/jwt/jwt.guard';
+import { User } from 'src/common/decorators/user.decorator';
+import { SuccessInterceptor } from 'src/common/interceptors/success.interceptor';
+import { UserEntity } from 'src/users/users.entity';
+import { ToiletAddDto } from '../dtos/toilet.add.dto';
+import { ToiletsService } from '../services/toilets.service';
 
-@Controller('controllers')
-export class ToiletsController {}
+@UseInterceptors(SuccessInterceptor)
+@Controller('api/toilets')
+export class ToiletsController {
+  constructor(private readonly toiletsService: ToiletsService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Post('additional')
+  async toiletAdditional(
+    @User() userInfo: UserEntity,
+    @Body() toiletAddDto: ToiletAddDto,
+  ) {
+    return await this.toiletsService.toiletAdditional(userInfo, toiletAddDto);
+  }
+}
