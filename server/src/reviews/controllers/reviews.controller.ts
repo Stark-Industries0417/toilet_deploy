18d56fe63ba5e014d3083860b43ef412f580e98f
Kwon <anstjdrnjs3@gmail.com>
2022-06-17 17:46:34 +0900

    add review api controller

--- server/src/reviews/controllers/reviews.controller.ts
+++ server/src/reviews/controllers/reviews.controller.ts
@@ -1,4 +1,43 @@
+import { Body } from '@nestjs/common';
+import { UseInterceptors } from '@nestjs/common';
+import { UploadedFile } from '@nestjs/common';
+import { UseGuards } from '@nestjs/common';
+import { Post } from '@nestjs/common';
 import { Controller } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import {
+  ApiBearerAuth,
+  ApiConsumes,
+  ApiOperation,
+  ApiResponse,
+} from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/auth/jwt/jwt.guard';
+import { AwsService } from 'src/aws.service';
+import { User } from 'src/common/decorators/user.decorator';
+import { SuccessInterceptor } from 'src/common/interceptors/success.interceptor';
+import { UserEntity } from 'src/users/users.entity';
+import { ReviewsService } from '../services/reviews.service';
 
-@Controller('controllers')
-export class ReviewsController {}
+@UseInterceptors(SuccessInterceptor)
+@Controller('api/reviews')
+export class ReviewsController {
+  constructor(
+    private readonly reviewsService: ReviewsService,
+    private readonly awsService: AwsService,
+  ) {}
+
+  @ApiOperation({
+    summary: '리뷰 추가 페이지',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'success: true, 리뷰 정보 반환',
+  })
+  @ApiConsumes('application/x-www-form-urlencoded')
+  @ApiBearerAuth('access-token')
+  @UseGuards(JwtAuthGuard)
+  @Post('additional')
+  async reviewAdditional(@User() user: UserEntity, @Body() reviewAddDto) {
+    return await this.reviewsService.additional(user.id, reviewAddDto);
+  }
+}
