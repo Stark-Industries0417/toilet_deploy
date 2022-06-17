c6f4901dd0e6b2cf82d7b53639191e77f8eaed63
Kwon <anstjdrnjs3@gmail.com>
2022-06-17 17:49:09 +0900

    modify review module

--- server/src/reviews/reviews.module.ts
+++ server/src/reviews/reviews.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AwsService } from 'src/aws.service';
+import { OptionsModule } from 'src/options/options.module';
+import { ToiletsModule } from 'src/toilets/toilets.module';
+import { UsersModule } from 'src/users/users.module';
 import { ReviewsController } from './controllers/reviews.controller';
+import { ReviewEntity } from './reviews.entity';
 import { ReviewsService } from './services/reviews.service';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([ReviewEntity]),
+    UsersModule,
+    ToiletsModule,
+    OptionsModule,
+  ],
   controllers: [ReviewsController],
-  providers: [ReviewsService],
+  providers: [ReviewsService, AwsService],
 })
 export class ReviewsModule {}
