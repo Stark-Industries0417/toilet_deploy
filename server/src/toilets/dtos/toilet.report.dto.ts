dcd218f6a97ad371fd16c2b83f8c5ce2485a56d5
Kwon <anstjdrnjs3@gmail.com>
2022-06-29 22:33:33 +0900

    toilet report dto

--- server/src/toilets/dtos/toilet.report.dto.ts
+++ server/src/toilets/dtos/toilet.report.dto.ts
@@ -1,7 +1,4 @@
 import { PickType } from '@nestjs/swagger';
 import { ToiletEntity } from '../toilets.entity';
 
-export class ToiletReportDto extends PickType(ToiletEntity, [
-  'address',
-  'stack',
-]) {}
+export class ToiletReportDto extends PickType(ToiletEntity, ['address']) {}
