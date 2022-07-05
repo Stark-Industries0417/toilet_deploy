47c410a4ab20e54350decdfb79b7e474c06620ce
Kwon <anstjdrnjs3@gmail.com>
2022-07-05 14:25:02 +0900

    화장실 별 리뷰 api service

--- server/src/reviews/services/reviews.service.ts
+++ server/src/reviews/services/reviews.service.ts
@@ -2,10 +2,12 @@ import { InternalServerErrorException } from '@nestjs/common';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { OptionEntity } from 'src/options/options.entity';
+import { ToiletReportDto } from 'src/toilets/dtos/toilet.report.dto';
 import { ToiletEntity } from 'src/toilets/toilets.entity';
 import { UserEntity } from 'src/users/users.entity';
 import { Repository } from 'typeorm';
 import { ReviewAddDto } from '../dtos/review.add.dto';
+import { ToiletsReivew } from '../dtos/review.toilet.dto';
 import { ReviewEntity } from '../reviews.entity';
 
 @Injectable()
@@ -74,6 +76,25 @@ export class ReviewsService {
       FROM toilet.REVIEW as review, toilet.TOILET as toilet
       WHERE review.author_id = '${userInfo.id}' and review.toilet_id = toilet.id
       ORDER BY review.created_at DESC
+      `);
+
+      return reviews;
+    } catch (err) {
+      throw new InternalServerErrorException(err.message);
+    }
+  }
+
+  async getToiletReview({
+    address,
+  }: ToiletReportDto): Promise<ToiletsReivew[]> {
+    try {
+      const reviews = await this.reviewsRepository.query(`
+        SELECT review.id, user.img_url, user.nickname, review.rate, 
+        DATE_FORMAT(CONVERT_TZ(review.created_at, 'UTC', 'Asia/Seoul'), '%Y/%m/%d') as time,
+        review.content
+        FROM toilet.REVIEW as review, toilet.USER as user, toilet.TOILET as toilet
+        WHERE review.toilet_id = toilet.id and review.author_id = user.id and toilet.address = '${address}'
+        ORDER by review.created_at DESC;
       `);
 
       return reviews;
