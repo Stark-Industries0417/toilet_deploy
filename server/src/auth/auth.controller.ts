486e908ebfe4faaa8f8c0e2dc5d1e5b14f0d508c
Kwon <anstjdrnjs3@gmail.com>
2022-07-11 19:42:47 +0900

    kakao oauth login api in controller

---
+++ server/src/auth/auth.controller.ts
@@ -0,0 +1,45 @@
+import { Controller, Get, UseGuards, UseInterceptors } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { User } from 'src/common/decorators/user.decorator';
+import { SuccessInterceptor } from 'src/common/interceptors/success.interceptor';
+import { UserResponseDto } from 'src/users/dtos/user.response.dto';
+import { AuthService } from './auth.service';
+import { KakaoAuthGuard } from './jwt/kakao.guard';
+import { KakaoRegisterDto } from './kakao.register.dto';
+
+@ApiTags('kakao')
+@UseInterceptors(SuccessInterceptor)
+@Controller('api/auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @ApiOperation({
+    summary: '/api/kakao/redirect로 리다이렉트 되는 api 입니다.',
+    description: '이 api로 카카오 로그인 버튼 연결해 주시면 됩니다.',
+  })
+  @ApiResponse({
+    status: 200,
+  })
+  @UseGuards(KakaoAuthGuard)
+  @Get('/kakao')
+  kakaoLogin() {
+    return;
+  }
+
+  @ApiOperation({
+    summary: '카카오 로그인한 사용자의 정보 반환하는 api',
+  })
+  @ApiResponse({
+    status: 200,
+    type: UserResponseDto,
+  })
+  @ApiResponse({
+    status: 409,
+    description: '이미 이메일로 가입한 경우 ConflictException 에러를 띄웁니다.',
+  })
+  @UseGuards(KakaoAuthGuard)
+  @Get('/kakao/redirect')
+  async kakaoLoginCallback(@User() kakao: KakaoRegisterDto) {
+    return this.authService.kakaoLogin(kakao);
+  }
+}
